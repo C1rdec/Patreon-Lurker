5f8e815d61706a7f25e0c3949fd6823e754da5c1
C1rdec <cedric_lampron@hotmail.com>
2023-05-27 14:44:00 -0400

    First commit

---
+++ src/Lurker.Patreon/TokenResult.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Lurker.Patreon;
+
+public class TokenResult
+{
+    /// <summary>
+    /// Gets or sets the access token.
+    /// </summary>
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of the token.
+    /// </summary>
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the expires in.
+    /// </summary>
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    /// <summary>
+    /// Gets or sets the refresh token.
+    /// </summary>
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the scope.
+    /// </summary>
+    [JsonPropertyName("scope")]
+    public string Scope { get; set; }
+}
---
+++ src/Lurkey.Patreon.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿using Lurker.Patreon;
+
+using var service = new PatreonService(new int[] { 8080, 8181, 8282 }, "<ClientId>");
+
+var tokenResult = await service.GetAccessTokenAsync();
+
+await service.IsPledging("<CampaignId>", tokenResult.AccessToken);
