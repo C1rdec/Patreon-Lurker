2a2e3b69dfa8df70e87f202e0c858e9cecb7fc0c
Cédric Lampron <cedric_lampron@hotmail.com>
2024-01-29 22:17:03 -0500

    Revert dev data

--- src/Lurkey.Patreon.Console/Program.cs
+++ src/Lurkey.Patreon.Console/Program.cs
@@ -3,11 +3,12 @@ using Lurker.Patreon.Models;
 
 var credential = new PatreonApiCredential
 {
-    ClientId = "uI0ZqaEsUckHlpQdOgnJfGtA9tjdKy4A9IpfJj9M2ZIMRkZrRZSemBJ2DtNxbPJm",
-    Ports = [8080, 8181, 8282]
+    ClientId = "<ClientID>",
+    Ports = [8080, 8181, 8282],
 };
 
 var service = new PatreonService(credential);
 await service.LoginAsync();
 
-var isPledged = await service.CheckPledgeStatusAsync("3779584");
+var isPledged = await service.CheckPledgeStatusAsync("<CampaignId>");
+Console.WriteLine(isPledged);
