f4dbdd34c90d4d69de3e2a514b6f886d2f41c12e
Cédric Lampron <cedric_lampron@hotmail.com>
2024-11-27 22:58:49 -0500

    Fix patreon service

--- src/Lurkey.Patreon.Console/Program.cs
+++ src/Lurkey.Patreon.Console/Program.cs
@@ -3,12 +3,12 @@ using Lurker.Patreon.Models;
 
 var credential = new PatreonApiCredential
 {
-    ClientId = "<ClientID>",
+    ClientId = "4fpNWVu2TEB5GaymlD5ENgxfiXNkLR48zCJ-a8wankXh0dtn-BEgYRibTFLDEkDo",
     Ports = [8080, 8181, 8282],
 };
 
 var service = new PatreonService(credential);
 await service.LoginAsync();
 
-var isPledged = await service.CheckPledgeStatusAsync("<CampaignId>");
+var isPledged = await service.CheckPledgeStatusAsync("3779584");
 Console.WriteLine(isPledged);
